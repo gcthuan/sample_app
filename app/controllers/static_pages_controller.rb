c0dfe02d36ff0c22070ca8c75df291d652e8fb33
Altair <gcthuan@apcs.vn>
2014-06-05 17:45:07 +0700

    Fix home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+
   def home
-  	@micropost  = current_user.microposts.build
-    @feed_items = current_user.feed.paginate(page: params[:page])
+  	if current_user.nil?
+  	  
+  	else
+  	  @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+	end
   end
 
   def help
